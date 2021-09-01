17e67b64bd65b0778cb9f7634a1ef253df04c9a1
Marko <marko@nonko.space>
2021-09-01 19:21:09 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
         <section id="entry" class="px-6 flex">
             <div class="flex flex-col h-screen justify-end text-white">
                 <h1 class="bg-clip-text neon neon-wide">
-                    <span class="block bg-clip-text text-transparent">Hallo, Mein Name ist Marko...<br /><br />Ich entwickle digitale Produkte</span>
+                    <span class="block bg-clip-text text-transparent">Hallo, Mein Name ist Marko<br /><br />Ich entwickle digitale Produkte</span>
                 </h1>
                 <p class="my-16"><a href="mailto:hello@lukacmarko.com">hello@lukacmarko.com</a></p>
             </div>
