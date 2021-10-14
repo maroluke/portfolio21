1b37b38d43fbc8521a9d82c92083c61bbefa159f
Marko <marko@nonko.space>
2021-10-14 23:21:45 +0200

    fix lang switch

--- resources/views/components/lang-switch.blade.php
+++ resources/views/components/lang-switch.blade.php
@@ -1,6 +1,6 @@
 <ul class="language flex gap-8">
     @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-        @if(LaravelLocalization::getCurrentLocale() !== $localeCode)
+        @if(!App::isLocale($localeCode))
             <li class="py-6">
                 <a class="py-6 opacity-75 text-white font-heading text-sm tracking-widest" rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
                     {{ ucfirst($properties['native']) }}
