3b4e1c14b484febc0daa26e3405f082ba3fb41be
Marko <marko@nonko.space>
2022-02-03 01:35:44 +0100

    route experiment

--- app/Http/Controllers/CLocale.php
+++ app/Http/Controllers/CLocale.php
@@ -9,7 +9,7 @@ class CLocale extends Controller
     public function setLocale($locale) {
         $clientLocale = locale_accept_from_http($_SERVER['HTTP_ACCEPT_LANGUAGE']);
 
-        if (isset($locale) && in_array($locale, config('app.available_locales'))) {
+        if (!empty($locale) && in_array($locale, config('app.available_locales'))) {
             app()->setLocale($locale);
         } elseif (in_array($clientLocale, config('app.available_locales'))) {
             app()->setLocale($clientLocale);
