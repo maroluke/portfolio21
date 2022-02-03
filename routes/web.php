d303a7cc68ad5870d417204512aacf89a444bb14
Marko <marko@nonko.space>
2022-02-03 14:10:00 +0100

    experimenting with routes

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 //     return view('welcome');
 // });
 
-Route::get('/{locale?}', function ($locale = null, $clientLocale = null) {
+Route::get('/{locale?}', function ($locale = null) {
     $clientLocale = locale_accept_from_http($_SERVER['HTTP_ACCEPT_LANGUAGE']);
 
     if (isset($locale) && in_array($locale, config('app.available_locales'))) {
