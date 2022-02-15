bb5a9632b1531ba89423c83e708eb5309895a33d
Marko <marko@nonko.space>
2022-02-15 22:07:44 +0100

    add share image

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,17 +1,18 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Hi there, My name is Marko. As an experienced software developer, I implement user-centric applications and meaningful websites for both mobile and desktop.">
+<meta name="keywords" content="Apps, Usability, Design, Testing">
+<meta name="author" content="Marko Lukac">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-<meta title="Marko Lukac – Portfolio 21" >
-<meta description="Hi there, My name is Marko. As an experienced software developer, I implement user-centric applications and meaningful websites for both mobile and desktop.">
+<title>Portfolio 21</title>
 
-<meta property="og:title" content="Marko Lukac – Portfolio 21">
+<meta property="og:title" content="Portfolio 21">
 <meta property="og:description" content="Hi there, My name is Marko. As an experienced software developer, I implement user-centric applications and meaningful websites for both mobile and desktop.">
 <meta property="og:image" content="https://markolukac.ch/media/memoji-sticker-07.png">
 <meta property="og:url" content="https://markolukac.ch/">
+
 <meta name="twitter:card" content="summary_large_image">
-<meta
-    name="twitter:image"
-    content="https://markolukac.ch/media/memoji-sticker-07.png"
+<meta name="twitter:image" content="https://markolukac.ch/media/memoji-sticker-07.png"
   />
 
 {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
