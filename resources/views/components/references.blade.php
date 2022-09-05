094d5fb6dc4b52ec87b742b069e0f46a4ad3eac5
maroluke <hello@markolukac.ch>
2022-09-05 21:10:59 +0200

    fix: backdrop-blur bug on firefox

--- resources/views/components/references.blade.php
+++ resources/views/components/references.blade.php
@@ -12,7 +12,7 @@
                     <div class="relative text-black bg-dirty-white rounded-2xl overflow-hidden border-gray-200 flex flex-col h-full transition-opacity duration-300 ease-in-out sm:border-none drop-shadow sm:grid sm:grid-cols-3 sm:p-0">
                         <div class="h-44 bg-cover bg-no-repeat image sm:bg-center sm:h-full sm:min-h-full sm:col-span-1" style="background-image: url({{ asset('media/mobile-screen-generation-purpose-01.png') }});"></div>
 
-                        <div class="reference-text backdrop-blur-xl rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
+                        <div class="reference-text rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
                             <h3 class="align-text-bottom text-small font-bolder">Generation Purpose</h3>
                             <p class="text-tiny py-6">@lang('Generation Purpose hilft Organi­sationen und Einzel­personen, ihren Purpose zu finden und diesen im Alltag zu imple­men­tieren.')</p>
                             <p class="text-tiny pb-2">@lang('In Zusam­men­arbeit mit den Gründern durfte ich einer­seits die ')<a class="bg-yellow-200 px-1 hover:bg-yellow-400 text-black sm:hover:text-black rounded" href="https://generation-purpose.org" target="_blank">@lang('Online–Präsenz')</a>@lang(' entwickeln sowie eine digitale Plat­tform aufbauen, auf welcher die Facili­tators (Mit­arbeiter) und Kunden täglich arbeiten können.')</p>
@@ -28,7 +28,7 @@
                     <div class="relative text-black bg-dirty-white rounded-2xl overflow-hidden border-gray-200 flex flex-col h-full transition-opacity duration-300 ease-in-out sm:border-none drop-shadow sm:grid sm:grid-cols-3 sm:p-0">
                         <div class="h-44 bg-cover bg-center image sm:h-full sm:min-h-full sm:col-span-1" style="background-image: url({{ asset('media/desktop-screen-fundament-02.png') }});"></div>
 
-                        <div class="reference-text backdrop-blur-xl rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
+                        <div class="reference-text rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
                             <h3 class="align-text-bottom text-small font-bolder">Fundament</h3>
                             <p class="text-tiny py-6">@lang('Das schweizer Investment–Startup Fundament AG, investiert in nach­haltige Ge­schäfts­modelle und Unter­nehmen.')</p>
                             <p class="text-tiny pb-2">@lang('Mit einem von mir selektierten Team sowie einem starken Fokus auf ')<a class="bg-yellow-200 px-1 hover:bg-yellow-400 text-black sm:hover:text-black rounded" href="https://fundament.swiss" target="_blank">@lang('Online–Präsenz')</a>@lang(' und digitale Zusam­menarbeit, wurde die Corporate Identity von Fundament und die dazu­gehörigen digitalen Plat­tformen entwickelt.')</p>
@@ -44,7 +44,7 @@
                     <div class="relative text-black bg-dirty-white rounded-2xl overflow-hidden border-gray-200 flex flex-col h-full transition-opacity duration-300 ease-in-out sm:border-none drop-shadow sm:grid sm:grid-cols-3 sm:p-0">
                         <div class="h-44 bg-cover bg-center image sm:h-full sm:min-h-full sm:col-span-1" style="background-image: url({{ asset('media/mobile-screen-coffaina-04.png') }});"></div>
 
-                        <div class="reference-text backdrop-blur-xl rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
+                        <div class="reference-text rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
                             <h3 class="align-text-bottom text-small font-bolder">coffaina</h3>
                             <p class="text-tiny py-6">@lang('Die wohl stärkste Cola der Welt. coffaina ist eine Berliner Indie-Cola, die bei ewigen Party­nächten, Abi–Stress oder einem 24–Stunden–Drag–Race zuver­lässig wach hält.')</p>
                             <p class="text-tiny pb-2">@lang('In einem desing-sprint-workshop in Berlin konnte ich mit dem Kunden den ')<a class="bg-yellow-200 px-1 hover:bg-yellow-400 text-black sm:hover:text-black rounded" href="http://coffaina.com" target="_blank">@lang('digitalen Auftritt')</a> @lang(' entwickeln und diesen in einem zweiten Schritt umsetzen.')</p>
@@ -60,7 +60,7 @@
                     <div class="relative text-black bg-dirty-white rounded-2xl overflow-hidden border-gray-200 flex flex-col h-full transition-opacity duration-300 ease-in-out sm:border-none drop-shadow sm:grid sm:grid-cols-3 sm:p-0">
                         <div class="h-44 bg-cover bg-center image sm:h-full sm:min-h-full sm:col-span-1" style="background-image: url({{ asset('media/desktop-screen-naanu-02.png') }});"></div>
 
-                        <div class="reference-text backdrop-blur-xl rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
+                        <div class="reference-text rounded-b-2xl p-5 flex flex-col flex-grow justify-start relative sm:p-5 sm:col-span-2">
                             <h3 class="align-text-bottom text-small font-bolder">naanu</h3>
 
                             @if (App::isLocale('zh'))
